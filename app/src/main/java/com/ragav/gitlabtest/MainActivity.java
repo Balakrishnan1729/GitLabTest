0c99a1e102209154412e620495477c303b47976e
Shanmugam B <shanmugam@kutung.in>
2017-08-08 12:53:21 +0530

    TestCommit 1

--- app/src/main/java/com/ragav/gitlabtest/MainActivity.java
+++ app/src/main/java/com/ragav/gitlabtest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Bala Test GitLab");
+        System.out.println("Bala Test GitLab 123");
     }
 }
